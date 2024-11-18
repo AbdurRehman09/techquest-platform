4f2e8e33284ae3e0421d45cfa5e41dc56cedb0f9
AbdurRehman09 <l216098@lhr.nu.edu.pk>
2024-11-18 13:51:22 +0500

    latest changes

--- app/lib/apollo-client.ts
+++ app/lib/apollo-client.ts
@@ -1,6 +1,22 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
+
+const httpLink = createHttpLink({
+  uri: '/api/graphql',
+  credentials: 'same-origin'
+})
 
 export const client = new ApolloClient({
-  uri: '/api/graphql',
+  link: httpLink,
   cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'network-only'
+    },
+    query: {
+      fetchPolicy: 'network-only'
+    },
+    mutate: {
+      fetchPolicy: 'no-cache'
+    }
+  }
 }) 
